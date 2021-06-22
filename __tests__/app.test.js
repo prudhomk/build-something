bf91524b54038fbd7b59cf888f2745342828b1ee
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-22 09:16:48 -0700

    added getAll route, test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,30 +9,37 @@ describe('demo routes', () => {
     return setup(pool);
   });
 
-  const gif = {
-    url: 'https://giphy.com/gifs/planet-globe-joe-winograd-HRzhhyrs8n5JFyCTsv',
-    title: 'Op Art GIF by Joe Winograd',
-    images: {
-      'hd': {
-        'height': '1400',
-        'mp4': 'https://media3.giphy.com/media/HRzhhyrs8n5JFyCTsv/giphy-hd.mp4?cid=e3b0c44280100a5538c01d0a5273334d0c0fb294cc9758dd&rid=giphy-hd.mp4&ct=g',
-        'mp4_size': '18317357',
-        'width': '1400'
-      },
-    }
-  };
-  
-
   it('creates a datapoint with a requested gif', async () => {
     const res = await request(app)
       .post('/api/v1/gifs');
       
-
     expect(res.body).toEqual({ 
       id: expect.any(String),
       url: expect.any(String),
       title: expect.any(String),
       image: expect.any(Object),
     });
+  });
+
+  it('receives gifs from table', async () => {
+    
+    const gif1 = await Gif.insert({
+      id: expect.any(String),
+      url: expect.any(String),
+      title: expect.any(String),
+      image: expect.any(Object)
+    });
+
+    const gif2 = await Gif.insert({
+      id: expect.any(String),
+      url: expect.any(String),
+      title: expect.any(String),
+      image: expect.any(Object)
+    });
+    
+    const res = await request(app)
+      .get('/api/v1/gifs');
+
+    expect(res.body).toEqual([gif1, gif2]);
   });
 });
